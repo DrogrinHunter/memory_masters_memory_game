3ecbc00d1c4f7c89d589ffb67287a5af9cd5bc54
DrogrinHunter <lukewillmore67@gmail.com>
2020-10-13 18:09:13 +0000

    added js code to compare cards to themselves. fixed bug of cards not disappearing when selected (if they were a match). the bug was due to incorrect spelling in if statement on line 114

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,47 +49,81 @@ const cardsArray = [
         img: 'assets/images/bootstrap.svg'
     }
 ]
+
+
+
+
 // duplicating 2 sets of cards 
-let gameGrid = cardsArray.concat(cardsArray);
+const gameGrid = cardsArray.concat(cardsArray);
 
 //randomising card order
 gameGrid.sort(() => 0.5 - Math.random());
 
+// global variables
+let firstGuess = ''
+let secondGuess = ''
+let count = 0;
+let previousTarget = null;
+
 // inputting the cards into the dom 
 const game = document.getElementById('game');
-
 const grid = document.createElement('section');
 grid.setAttribute('class', 'grid');
-
 game.appendChild(grid);
 
+// creating divs for cards 
 gameGrid.forEach((item) => {
     const card = document.createElement('div');
-
     card.classList.add('card');
-
     card.dataset.name = item.name;
+    
 
     card.style.backgroundImage = `url(${item.img})`;
 
     grid.appendChild(card)
 })
 
+const match = () => {
+    var selected = document.querySelectorAll('.selected');
+    selected.forEach((card) => {
+        card.classList.add('match');
+    });
+}
+
 // event listener for when a card is selected 
 grid.addEventListener('click', function(event){ 
     let clicked = event.target;
-    if (clicked.nodeName === 'SECTION') {
+    if (clicked.nodeName === 'SECTION' || clicked === previousTarget) {
         return
     }
 
-    clicked.classList.add('selected')
+    clicked.classList.add('selected');
+
+    if (count < 2 ) {
+        count++;
+
+        if (count === 1) {
+            firstGuess = clicked.dataset.name;
+            clicked.classList.add('selected');
+        } else {
+            secondGuess = clicked.dataset.name
+            clicked.classList.add('selected')
+        };
+
+        if (firstGuess !== '' && secondGuess !== '') {
+            if (firstGuess === secondGuess) {
+                match()
+            }
+        }
+        previousTarget = clicked;
+    }
 })
 
-// allowing only 2 cards to be selected at a time
-let count = 0;
 
-if (count < 2) {
-    count++;
 
-    clicked.classList.add('selected');
-}
+// if (count < 2) {
+//     count++;
+
+//     clicked.classList.add('selected');
+// };
+
